d4dbeee7d025bc32812a562877f167fcaf7b442d
Daniel Sapkota <daniel@waterbot.xyz>
2020-12-29 14:37:08 +0545

    A fix

--- frontend_interface/views.py
+++ frontend_interface/views.py
@@ -383,6 +383,8 @@ def daddy_interface(request):
 
             if free_balance != 0:
                 row['balance'] = free_balance + 0.003
+            else:
+                row['balance'] = free_balance
             
             new_df.append(row.to_dict())
 
