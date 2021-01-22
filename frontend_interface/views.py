6b458e7908ca93e48560fb99bdc4ff01cef248ee
Daniel Sapkota <daniel@waterbot.xyz>
2021-01-22 08:20:34 +0545

    Fix nissan

--- frontend_interface/views.py
+++ frontend_interface/views.py
@@ -69,7 +69,7 @@ def nissan(request):
         details_df = get_positions()
         altcoin_balance = details_df['ftx_balance'].sum()
 
-        pnl = altcoin_balance - 2500
+        pnl = altcoin_balance - 3500
 
         if pnl > 0:
             pnl = pnl / (pnl/random.randint(130,150))
