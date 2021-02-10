c1ce211dfc938fe91fceee213e88737b39ed0c0f
Daniel Sapkota <daniel@waterbot.xyz>
2021-02-10 11:34:17 +0545

    Fix

--- daddy.py
+++ daddy.py
@@ -47,42 +47,43 @@ if __name__ == "__main__":
     bot_thread.start()
 
     for idx, row in EXCHANGES.iterrows():
-        exchange = row['cryptofeed_name']
-        b = getattr(exchanges, exchange)
+        if row['trade'] == 1 or row['name'] == 'bitmex':
+            exchange = row['cryptofeed_name']
+            b = getattr(exchanges, exchange)
 
-        pairs = [row['cryptofeed_symbol']]
+            pairs = [row['cryptofeed_symbol']]
 
-        trade_callbacks = []
-        obook_callbacks = []
-        ticker_callbacks = []
+            trade_callbacks = []
+            obook_callbacks = []
+            ticker_callbacks = []
 
-        if row['types'] != "":
-            for type in row['types'].split(";"):
-                if 'stream' in type:
-                    trade_callbacks.append(daddy_trade)
+            if row['types'] != "":
+                for type in row['types'].split(";"):
+                    if 'stream' in type:
+                        trade_callbacks.append(daddy_trade)
 
-                if 'book' in type:
-                    obook_callbacks.append(daddy_book)
+                    if 'book' in type:
+                        obook_callbacks.append(daddy_book)
 
-                if 'ticker' in type:
-                    ticker_callbacks.append(daddy_ticker)
+                    if 'ticker' in type:
+                        ticker_callbacks.append(daddy_ticker)
 
-            channels = []
-            callbacks = {}
+                channels = []
+                callbacks = {}
 
-            if len(trade_callbacks) > 0:
-                channels.append(TRADES)
-                callbacks[TRADES] = trade_callbacks
+                if len(trade_callbacks) > 0:
+                    channels.append(TRADES)
+                    callbacks[TRADES] = trade_callbacks
 
-            if len(obook_callbacks) > 0:
-                channels.append(L2_BOOK)
-                callbacks[L2_BOOK] = obook_callbacks
+                if len(obook_callbacks) > 0:
+                    channels.append(L2_BOOK)
+                    callbacks[L2_BOOK] = obook_callbacks
 
-            if len(ticker_callbacks) > 0:
-                channels.append(TICKER)
-                callbacks[TICKER] = ticker_callbacks
+                if len(ticker_callbacks) > 0:
+                    channels.append(TICKER)
+                    callbacks[TICKER] = ticker_callbacks
 
-            print("{} {} {}".format(pairs, callbacks, channels))
-            f.add_feed(b(pairs=pairs, channels=channels, callbacks=callbacks), timeout=-1)
+                print("{} {} {}".format(pairs, callbacks, channels))
+                f.add_feed(b(pairs=pairs, channels=channels, callbacks=callbacks), timeout=-1)
 
     f.run()
