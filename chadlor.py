abc40d167bd63b0d61512457c1a0fbea2b6ff8aa
Daniel Sapkota <daniel@waterbot.xyz>
2021-02-27 21:59:19 +0545

    A fix

--- chadlor.py
+++ chadlor.py
@@ -6,11 +6,17 @@ import threading
 
 from algos.chadlor.bot import chadlor_bot, chadlor_trade
 
+import os
+
+
 if __name__ == "__main__":
+    if os.path.isfile("logs/chadlor_bot.log"):
+        os.remove("logs/chadlor_bot.log")
+
     bot_thread = threading.Thread(target=chadlor_bot)
     bot_thread.start()
 
-f = FeedHandler(retries=100000)
-f.add_feed(Bitmex(pairs=['XBTUSD'], channels=[TRADES], callbacks={TRADES: chadlor_trade}), timeout=-1)
-f.add_feed(Coinbase(pairs=['BTC-USD'], channels=[TRADES], callbacks={TRADES: chadlor_trade}), timeout=-1)
-f.run()
+    f = FeedHandler(retries=100000)
+    f.add_feed(Bitmex(pairs=['XBTUSD'], channels=[TRADES], callbacks={TRADES: chadlor_trade}), timeout=-1)
+    f.add_feed(Coinbase(pairs=['BTC-USD'], channels=[TRADES], callbacks={TRADES: chadlor_trade}), timeout=-1)
+    f.run()
