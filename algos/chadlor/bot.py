17ec4144dd77c9b417904d642631fe7b9d447b1f
Daniel Sapkota <daniel@waterbot.xyz>
2021-02-28 09:34:28 +0545

    Fix try

--- algos/chadlor/bot.py
+++ algos/chadlor/bot.py
@@ -35,13 +35,14 @@ def get_coinbase_api():
     cbase = pd.DataFrame(json.loads(requests.get("https://api.pro.coinbase.com/products/BTC-USD/candles?granularity=60").text))
     cbase.columns = ['timestamp', 'low', 'high', 'open', 'close', 'volume']
     cbase['timestamp'] = pd.to_datetime(cbase['timestamp'], unit='s')
+    cbase = cbase[['timestamp', 'close']]
     return cbase
 
 def get_bitmex_api():
     bmex = pd.DataFrame(json.loads(requests.get("https://www.bitmex.com/api/v1/trade/bucketed?binSize=1m&symbol=XBTUSD&count=500&reverse=true").text))
     bmex['timestamp'] = pd.to_datetime(bmex['timestamp'])
     bmex['timestamp'] = bmex['timestamp'].dt.tz_localize(None)
-    bmex = bmex[['timestamp', 'low', 'high', 'open', 'close', 'volume']]
+    bmex = bmex[['timestamp', 'close']]
     return bmex
 
 def get_prices(endTime):
