e9b7040ff4a98c15c00e677682b2c524b9c740f5
Daniel Sapkota <daniel@waterbot.xyz>
2021-05-01 16:42:26 +0545

    now try

--- algos/daddy/restart.py
+++ algos/daddy/restart.py
@@ -3,8 +3,20 @@ import os
 if os.path.isfile('data/XBT_features.csv'):
     os.remove('data/XBT_features.csv')
 
+if os.path.isfile('data/features.csv'):
+    os.remove('data/features.csv')
+
 from arctic import Arctic
 
 store = Arctic('localhost')
 library = store['daddy']
-library.delete('XBT_trades')
+
+try:
+    library.delete('XBT_trades')
+except:
+    pass
+
+try:
+    library.delete('trades')
+except:
+    pass
